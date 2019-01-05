259cb42a8614377e58e7b5c8bb4d20d37222167f
sitmn <sho-t-jpx375@docomo.ne.jp>
2019-01-05 21:24:02 +0900

    Lesson6 課題途中
    UnityChanオブジェクトの子オブジェクトItemDestroyAreaをつくり、子オブジェクトに衝突したものを削除しようとしたが、衝突により親オブジェクトのスクリプトも実行されてしまう

---
+++ Assets/Scripts/ItemDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider Item)
+    {
+        if(Item.gameObject.tag == "CarTag" || Item.gameObject.tag == "TrafficConeTag" || Item.gameObject.tag == "CoinTag"){
+            Destroy(Item.gameObject);
+            Debug.Log("a");
+        }
+    }
+}
